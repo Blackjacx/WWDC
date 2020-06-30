3f99d33003232a5204b825014bccbd9011b3e080
Stefan Herold <stefan.herold@gmail.com>
2020-06-30 12:28:05 +0200

    Update progress to show the total

--- progress.sh
+++ progress.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 
 DONE=$(cat README.md | grep '1. \[' |wc -l |sed 's/^[ ]*//')
 REMAINING=$(cat README.md | grep '1. \*\*(TO-DO)\*\*' |wc -l |sed 's/^[ ]*//')
-echo "✅ $DONE / 👨‍💻 $REMAINING"
+TOTAL=$((REMAINING+DONE))
+echo "✅ $DONE / 👨‍💻 $TOTAL"
 
 echo "Updating README.md progress badge..."
 
-BADGE_URL="https://progress-bar.dev/$DONE/?scale=$REMAINING&title=Progress&width=600&suffix=%20/%20$REMAINING%20Sessions"
+BADGE_URL="https://progress-bar.dev/$DONE/?scale=$REMAINING&title=Progress&width=600&suffix=%20/%20$TOTAL%20Sessions"
 SED_ESCAPED_BADGE_URL=$(printf '%s\n' "$BADGE_URL" | sed -e 's/[\/&]/\\&/g')
 
 sed -Ei '' "s/(\!\[Progress\])(.*)/\1($SED_ESCAPED_BADGE_URL)/g" README.md
